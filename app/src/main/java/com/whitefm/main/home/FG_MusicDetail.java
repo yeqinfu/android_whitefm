1c1d611292a6f904ac538698a06e88b504c438c6
yeqinfu <990761790@qq.com>
2016-09-20 18:35:29 +0800

    !

---
+++ app/src/main/java/com/whitefm/main/home/FG_MusicDetail.java
@@ -0,0 +1,19 @@
+package com.whitefm.main.home;
+
+import com.whitefm.R;
+import com.whitefm.basefm.FG_BaseFM;
+
+/**
+ * Created by yeqinfu on 9/20/16.
+ */
+public class FG_MusicDetail extends FG_BaseFM {
+    @Override
+    protected int getFgRes() {
+        return R.layout.fg_music_detail;
+    }
+
+    @Override
+    protected void afterViews() {
+
+    }
+}
